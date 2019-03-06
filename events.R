fe38bb42f1e1f3673a67b61a36018fbdba151d21
Taavi Päll <tapa741@gmail.com>
2019-03-06 13:32:49 +0200

    simulate

--- events.R
+++ events.R
@@ -19,16 +19,48 @@ x[near(0.95, pe, tol = 0.0001)]
 #' Bayesian estimator
 #' if we use beta prior with parameters a and b we can use formula (see Razzaghi, 2002):
 #' Razzaghi, M. (2002). On the estimation of binomial success probability with zero occurrence in sample. Journal of Modern Applied Statistical Methods, 1(2), 41. http://digitalcommons.wayne.edu/cgi/viewcontent.cgi?article=1673&context=jmasm
-a <- 0.5
-b <- 0.5
-hist(rbeta(1000, a, b))
-upper_bound <- function(x) {
-    a / (a + b + x)
-    }
 
-xub <- upper_bound(x)
-ggplot(tibble(x, xub)) +
-    geom_line(aes(x, xub, group = 1)) +
-    geom_vline(xintercept = 138, linetype = "dashed")
-upper_bound(N) * 1e5
-upper_bound(500) * 100000
+eb_estimate <- function(x, a, b) {
+    a / (a + b + x)
+}
+
+#'
+#' Blood donors
+bd <- 2 / 1e6
+#' Sweden
+sw <- 0.2 / 1e5
+#' Our data
+we <- list(pos = 0, ntry = 138)
+#' IVF patients
+ivf <- 2.3 / 1e4
+#' injecting drug users
+idu <- 3.2 / 100
+
+#' Create prior parameters
+obs <- c(bd, sw, ivf, idu)
+values <- sample(obs, 1000, replace = TRUE)
+m <- MASS::fitdistr(values, dbeta, start = list(shape1 = 1, shape2 = 10))
+
+alpha0 <- m$estimate[1]
+beta0 <- m$estimate[2]
+
+#' Plot prior distribution
+set.seed(11)
+x <- seq(0.001, 0.1, 0.001)
+y <- dbeta(x, alpha0, beta0)
+tibble(x, y) %>% 
+    ggplot() +
+    geom_line(aes(x, y, group = 1)) +
+    labs(title = "Prior distribution")
+
+#' Estimate upper bound
+x <- seq(1, 1000, by = 1)
+xub <- eb_estimate(x, alpha0, beta0)
+
+#' Plot upper bound estimate
+as_tibble(cbind(x, p = xub)) %>% 
+    ggplot() +
+    geom_line(aes(x, p, group = 1)) +
+    geom_vline(xintercept = 138, linetype = "dashed") +
+    labs(y = "Probability with zero occurrence",
+         x = "Sample size")
